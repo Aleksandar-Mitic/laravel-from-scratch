dac2267b927129e020014121b1c25d42219bac84
Aleksandar Mitic <aleksandar.webart@gmail.com>
2019-05-16 16:39:34 +0200

    App name changed

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace BasicLaravel\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use BasicLaravel\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
--- config/app.php
+++ config/app.php
@@ -169,11 +169,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        BasicLaravel\Providers\AppServiceProvider::class,
+        BasicLaravel\Providers\AuthServiceProvider::class,
+        // BasicLaravel\Providers\BroadcastServiceProvider::class,
+        BasicLaravel\Providers\EventServiceProvider::class,
+        BasicLaravel\Providers\RouteServiceProvider::class,
 
     ],
 
